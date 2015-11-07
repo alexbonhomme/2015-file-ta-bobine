85f59a33be6d0f3875200a2e83027efd895980fd
Alexandre Bonhomme <alexand.bonhom@gmail.com>
2015-11-07 22:49:29 +0100

    Adds angular template

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name fileTaBobineApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the fileTaBobineApp
+ */
+angular.module('fileTaBobineApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name fileTaBobineApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the fileTaBobineApp
+ */
+angular.module('fileTaBobineApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
